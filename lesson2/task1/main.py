8eab70dbf22efa318bb4c8c8fef5765678cf530a
Владислав Хоренко <92306080+vlad-horenko@users.noreply.github.com>
2022-11-06 23:39:14 +0200

    Update main.py

--- lesson2/task1/main.py
+++ lesson2/task1/main.py
@@ -1,1 +1,13 @@
+import numpy as np
+import math
 
+n = int(input('Enter: '))
+
+A = np.random.uniform(low=-1, high=1, size=(n,2))
+count = 0
+for index, element in enumerate(A):
+    hypotenuse = math.sqrt(A[index][0] ** 2 + A[index][1] ** 2)
+    if hypotenuse <= 1:
+        count +=1
+print(f'{count} points are included in the unit circle from all given points.')
+print(f'X = {count*4/n}')
